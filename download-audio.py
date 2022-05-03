b239e64f8149fb5ed064bcffcad2e6d64eeda3e1
kempanator <ty2025@att.net>
2022-05-03 12:27:15 -0500

    Add files via upload

--- download-audio.py
+++ download-audio.py
@@ -10,11 +10,27 @@ except ModuleNotFoundError:
     subprocess.Popen(["python", "-m", "pip", "install", "-U", 'eyed3']).wait()
     import eyed3
 
+
+# return true if a dictionary key exists and has valid data
+def valid_key(d, k):
+    if k in d and d[k] is not None and d[k].strip() != "":
+        return True
+    else:
+        return False
+
+
 mode = 'audio'  # audio, video, both
 path = ''  # optional custom path
 json_path = path + 'json/'
 audio_path = path + 'audio/'
 video_path = path + 'video/'
+key_720 = 'sept'
+key_480 = 'quatre'
+key_mp3 = 'mptrois'
+key_anime = 'Anime'
+key_type = 'Type'
+key_artist = 'Artist'
+key_song = 'SongName'
 if not os.path.isdir(json_path):
     os.mkdir(json_path)
 
@@ -24,41 +40,41 @@ for file_name in os.listdir(json_path):
         data = json.load(json_file)
         json_file.close()
         for x in data:
-            if x['sept'] is None and x['quatre'] is None and x['mptrois'] is None:
+            if not valid_key(x, key_720) and not valid_key(x, key_480) and not valid_key(x, key_mp3):
                 pass
             else:
                 if mode == 'audio' or mode == 'both':
                     if not os.path.isdir(audio_path):
                         os.mkdir(audio_path)
-                    if x['mptrois'] is not None and 'catbox.moe' in x['mptrois']:
-                        new_file_name = x['mptrois'].split("/")[-1]
+                    if valid_key(x, key_mp3) and x[key_mp3].endswith('.mp3'):
+                        new_file_name = x[key_mp3].split("/")[-1]
                         if not os.path.isfile(audio_path + new_file_name):
-                            print(new_file_name + ' ' + x['Anime'] + ' - ' + x['Type'])
-                            urllib.request.urlretrieve(x['mptrois'], audio_path + new_file_name)
+                            print(new_file_name + ' - ' + x[key_anime] + ' - ' + x[key_type])
+                            urllib.request.urlretrieve(x[key_mp3], audio_path + new_file_name)
                             id3 = eyed3.load(audio_path + new_file_name)
                             id3.initTag()
-                            id3.tag.artist = x['Artist']
-                            id3.tag.title = x['SongName']
-                            id3.tag.comment = x['Type']
-                            id3.tag.album = x['Anime']
+                            id3.tag.artist = x[key_artist]
+                            id3.tag.title = x[key_song]
+                            id3.tag.comment = x[key_type]
+                            id3.tag.album = x[key_anime]
                             id3.tag.save()
                     else:
-                        print('Missing mp3: ' + x['Anime'] + ', ' + x['Type'] + ', ' + x['Artist'] + ', ' + x['SongName'])
+                        print('Missing mp3: ' + x[key_anime] + ', ' + x[key_type] + ', ' + x[key_artist] + ', ' + x[key_song])
                 if mode == 'video' or mode == 'both':
                     if not os.path.isdir(video_path):
                         os.mkdir(video_path)
                     link = ""
-                    if x['quatre'] is not None:
-                        link = x['quatre']
-                    if x['sept'] is not None:
-                        link = x['sept']
+                    if valid_key(x, key_480):
+                        link = x[key_480]
+                    if valid_key(x, key_720):
+                        link = x[key_720]
                     if link.startswith('http'):  # source can be catbox.moe or openings.moe
                         new_file_name = link.split("/")[-1]
                         if not os.path.isfile(video_path + new_file_name):
-                            print(new_file_name + ' - ' + x['Anime'] + ' - ' + x['Type'])
+                            print(new_file_name + ' - ' + x[key_anime] + ' - ' + x[key_type])
                             urllib.request.urlretrieve(link, video_path + new_file_name)
                     else:
-                        print('Missing video: ' + x['Anime'] + ', ' + x['Type'] + ', ' + x['Artist'] + ', ' + x['SongName'])
+                        print('Missing video: ' + x[key_anime] + ', ' + x[key_type] + ', ' + x[key_artist] + ', ' + x[key_song])
 
 if len(os.listdir(json_path)) == 0:
     print('Error: no json files found')
--- update-database.py
+++ update-database.py
@@ -1,14 +1,30 @@
 # convert json files from anisongdb.com into js dictionary
-# {listname: [["videolink", "audiolink", "anime", "type", "artist", "song"], ...]}
+# {listname: [["videolink" "audiolink", "anime", "type", "artist", "song"], ...]}
 
 import os
 import json
 
+
+# return true if a dictionary key exists and has valid data
+def valid_key(d, k):
+    if k in d and d[k] is not None and d[k].strip() != "":
+        return True
+    else:
+        return False
+
+
 path = ''  # optional custom path
-if not os.path.isdir(path + 'json'):
-    os.mkdir(path + 'json')
+key_720 = 'sept'
+key_480 = 'quatre'
+key_mp3 = 'mptrois'
+key_anime = 'Anime'
+key_type = 'Type'
+key_artist = 'Artist'
+key_song = 'SongName'
 dictionary = {}
 total_songs = 0
+if not os.path.isdir(path + 'json'):
+    os.mkdir(path + 'json')
 
 for file_name in os.listdir(path + 'json/'):
     if file_name[-5:] == '.json':
@@ -18,29 +34,31 @@ for file_name in os.listdir(path + 'json/'):
         data = json.load(json_file)
         json_file.close()
         for x in data:
-            if x['sept'] is None and x['quatre'] is None and x['mptrois'] is None:
+            if not valid_key(x, key_720) and not valid_key(x, key_480) and not valid_key(x, key_mp3):
                 pass
-            elif x['mptrois'] is not None and 'catbox.moe' in x['mptrois']:
+            elif valid_key(x, key_mp3) and x[key_mp3].endswith('.mp3'):
                 video = ""
-                if x['quatre'] is not None:
-                    video = x['quatre']
-                if x['sept'] is not None:
-                    video = x['sept']
-                audio = x['mptrois']
-                anime = x['Anime'].replace('"', '')
-                type = x['Type']
-                artist = x['Artist'].replace('"', '')
-                song = x['SongName'].replace('"', '')
+                if valid_key(x, key_480):
+                    video = x[key_480]
+                if valid_key(x, key_720):
+                    video = x[key_720]
+                if video == "":
+                    print('Missing video: ' + x[key_anime] + ', ' + x[key_type] + ', ' + x[key_artist] + ', ' + x[key_song])
+                audio = x[key_mp3]
+                anime = x[key_anime].replace('"', '')
+                type = x[key_type]
+                artist = x[key_artist].replace('"', '')
+                song = x[key_song].replace('"', '')
                 song_list.append((video, audio, anime, type, artist, song))
                 total_songs += 1
             else:
-                print('Missing mp3: ' + x['Anime'] + ', ' + x['Type'] + ', ' + x['Artist'] + ', ' + x['SongName'])
+                print('Missing mp3: ' + x[key_anime] + ', ' + x[key_type] + ', ' + x[key_artist] + ', ' + x[key_song])
         song_list = sorted(set(song_list), key=lambda i: str(i[2] + ' ' + i[3]).lower())  # remove duplicates and sort
         dictionary.update({list_name: song_list})
 
 total_lists = len(dictionary)
 if total_lists == 0:
-    print('error: nothing found')
+    print('Error: nothing found')
 else:
     print('total lists = ' + str(total_lists))
     print('total songs = ' + str(total_songs))
